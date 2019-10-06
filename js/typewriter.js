dc6f5b5846186964b7fd07609c25542d69a83589
Gabriel Zsigmond <gaazsig@gmail.com>
2019-10-06 17:04:06 -0300

    fixed toggle elements bug on mobile

---
+++ js/typewriter.js
@@ -0,0 +1,13 @@
+function typeWriter(txt) {
+  let i = 0;
+  let speed = 15; /* The speed/duration of the effect in milliseconds */
+  let printing = document.getElementById("comunication");
+  printing.innerHTML = ''
+  let typingId = setInterval(() => {
+    if (i < txt.length) {
+      printing.innerHTML += txt.charAt(i);
+      i++;
+    } else {clearInterval(typingId)}
+  }, speed);
+}
+typeWriter("");
